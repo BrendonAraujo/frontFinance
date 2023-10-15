2fac1d40663b0149a61aae13075b71d390908137
BrendonAraujo <brendong.araujo@gmail.com>
2023-10-15 15:43:08 -0300

    #REMOVE Cors

--- front/src/main.ts
+++ front/src/main.ts
@@ -20,13 +20,13 @@ import router from '@/router/index'
 const route  = router;
 const app = createApp(App)
 
-const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-  }
-  app.use(cors(corsOptions))
+// const cors = require('cors');
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,
+//     optionSuccessStatus: 200
+//   }
+//   app.use(cors(corsOptions))
 
 app.use (Hotjar, {
   id: '3694790' // Hotjar Site ID
