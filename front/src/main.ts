f6f90abe2ee2d20d2f924fc22d04ffb06461da1f
BrendonAraujo <brendong.araujo@gmail.com>
2023-10-15 08:27:31 -0300

    #ADD - HotJr

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,6 +10,7 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 // Composables
 import { createApp } from 'vue'
+import Hotjar from 'vue-hotjar'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
@@ -18,6 +19,18 @@ import router from '@/router/index'
 
 const route  = router;
 const app = createApp(App)
+
+// const cors = require('cors');
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,
+//     optionSuccessStatus: 200
+//   }
+//   app.use(cors(corsOptions))
+
+app.use (Hotjar, {
+  id: '3694780' // Hotjar Site ID
+})
 
 app.component('VueDatePicker', VueDatePicker);
 registerPlugins(app)
